ddcbbfac591ae69bad4071b0a69c8b4a1e4e892e
Yuliia <julynewbox@gmail.com>
2026-03-03 00:25:04 +0200

    Yaml, changes to publish report

--- playwright.config.js
+++ playwright.config.js
@@ -33,14 +33,20 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }], 
+             ['junit', { outputFile: 'junit-report/test-junit.xml' }],
+             ['list'],
+             ['json', { outputFile: 'test-results.json' }]
+            ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL,
+    screenshot: 'on',
+    video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     httpCredentials: {
       // @ts-ignore
       username: process.env.HTTP_CREDENTIALS_USERNAME,
