e27a531ad41166e4a468d9f835404e798e79fc27
Julykov <Julykov@users.noreply.github.com>
2026-03-02 23:07:58 +0000

    deploy: b74d694409f6cba4ac7d82f82dbfa0f6e43e6738

--- tests/hw_28/test.login.lesson.28.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from './my_test.js';
-import { expect } from '@playwright/test';
-
-
-test('Add one more car', async ({ userGaragePage, page }) => {
-    await expect(userGaragePage.profileDropdown).toBeVisible()
-    await expect(userGaragePage.updateMileageRecent).toBeVisible()
-})
